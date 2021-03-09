7a9f62844ea9574f9e84db64b9518f9bdefb5be3
aksmf1442 <aksmf1442@naver.com>
2021-03-09 22:45:40 +0900

    Refactor : user update
    기존에는 비밀번호만 알고있으면 아무나 유저의 정보를 변경할 수 있었는데, 그 부분을 로그인 후 자신의 정보만 수정할 수 있도록 변경했다.

--- src/main/java/com/codessquad/qna/controller/UserController.java
+++ src/main/java/com/codessquad/qna/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -54,30 +55,46 @@ public class UserController {
         return "/user/profile";
     }
 
-    @GetMapping("/{userId}/form")
-    public String updateUserForm(@PathVariable String userId, Model model) {
-        User user = userService.findUserByUserId(userId);
-        if (user == null) {
+    @GetMapping("/{id}/form")
+    public String updateUserForm(@PathVariable Long id, Model model, HttpSession session) {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+
+        if (sessionUser == null) {
+            return "redirect:/users/login";
+        }
+
+        if (!sessionUser.isMatchingId(id)) {
             return "redirect:/users";
         }
+
+        User user = userService.findById(id);
         model.addAttribute("user", user);
         return "/user/updateForm";
     }
 
     @PutMapping("/{id}")
-    public String updateUserProfile(@PathVariable Long id, User user,
-        PasswordVerifier passwordVerifier, RedirectAttributes redirect) {
+    public String updateUser(@PathVariable Long id, User user,
+        PasswordVerifier passwordVerifier, RedirectAttributes redirect, HttpSession session) {
+
+        User sessionUser = (User) session.getAttribute("sessionUser");
+
+        if (sessionUser == null) {
+            return "redirect:/users/login";
+        }
+
+        if (!sessionUser.isMatchingId(id)) {
+            return "redirect:/users";
+        }
 
         User originUser = userService.findById(id);
 
         if (originUser.confirmPassword(passwordVerifier)) {
-            user.setPassword(passwordVerifier.getReceivedPassword());
             userService.updateUserData(originUser, user);
             return "redirect:/users";
         }
 
         redirect.addFlashAttribute("fail", true);
-        return "redirect:/users/" + originUser.getUserId() + "/form";
+        return "redirect:/users/" + originUser.getId() + "/form";
     }
 
     @GetMapping("/logout")
