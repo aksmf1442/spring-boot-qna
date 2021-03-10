c7f3dff29b5d64b006a8de55b40f9274615ea172
aksmf1442 <aksmf1442@naver.com>
2021-03-10 12:38:27 +0900

    Refactor : question
    로그인한 유저만 질문하기 기능을 할 수 있도록 변경했다. 로그인하지 않은 유저가 질문하기 기능에 접근하면 로그인 화면으로 리다이렉션 해주도록
    하였고, 질문 리스트 화면에서 user의 name을 클릭하면 유저 프로필을 볼 수 있도록 기능을 추가했다.

--- src/main/java/com/codessquad/qna/service/UserService.java
+++ src/main/java/com/codessquad/qna/service/UserService.java
@@ -3,7 +3,6 @@ package com.codessquad.qna.service;
 import com.codessquad.qna.domain.User;
 import com.codessquad.qna.repository.UserRepository;
 import java.util.List;
-import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Service;
 
 @Service
